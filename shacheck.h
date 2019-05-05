d315e197643904ed4bcd4d357148606c9f204f31
Valentin Hilbig <webmaster@scylla-charybdis.com>
2019-05-05 15:45:46 +0200

    shacheck.h added

---
+++ shacheck.h
@@ -0,0 +1,5 @@
+/* This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+ */
+
+#define SHACHECK_ZMQ_DEFAULT    "tcp://127.0.0.1:1354"  /* 0x54a        */
