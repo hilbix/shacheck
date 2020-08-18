30deccd6f3b7a90a31b401987f085d75e841c1e6
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-08-18 18:22:16 +0200

    oops.h added

---
+++ oops.h
@@ -0,0 +1,34 @@
+#define	_GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#ifndef	STATIC
+#define	STATIC
+#endif
+
+STATIC void                                                                                                            
+VOOPS(int e, const char *s, va_list list)
+{
+  fprintf(stderr, "OOPS: ");
+  vfprintf(stderr, s, list);
+  if (e)
+    fprintf(stderr, ": %s", strerror(e));
+  fprintf(stderr, "\n");
+  exit(23);
+}
+
+STATIC void
+OOPS(const char *s, ...)
+{
+  va_list	list;
+  int		e = errno;
+
+  va_start(list, s);
+  VOOPS(e, s, list);
+  va_end(list);
+}
+
